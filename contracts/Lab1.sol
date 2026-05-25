5e6742170ec3c156191b2c1cc6920e4072774d22
cmt801107 <cmt801107@gmail.com>
2026-05-25 17:02:05 +0800

    first commit

---
+++ contracts/Lab1.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.7.4;
+contract Chat {
+    string public message;
+
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns(string memory) {
+        return message;
+    }
+}
