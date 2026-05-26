76e3cc2c1f8b45e869192a7f4a0e6b1f14a3a756
cmt801107 <cmt801107@gmail.com>
2026-05-26 10:57:53 +0800

    remove get message

--- contracts/Lab1.sol
+++ contracts/Lab1.sol
@@ -10,8 +10,4 @@ contract Chat {
     function setMessage(string memory newMessage) public {
         message = newMessage;
     }
-
-    function getMessage() public view returns(string memory) {
-        return message;
-    }
 }
